883064c9befe1802c3b08ffc45c4e5e3ee790894
Ilya Lekomcev <lekomtzev.ilja@gmail.com>
2021-04-08 19:20:32 +0500

    add timer

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ class webPlayer {
         this.index = null;
         this.playingTrack = null;
         this.allTracks = null;
+        this.currentTime = null;
+        this.totalTime = null;
 
         this.initVaribales();
         this.loadTrack(this.index);
@@ -36,6 +38,8 @@ class webPlayer {
         this.totalCount = document.getElementById('total_number');
         this.index = 0;
         this.playingTrack = false;
+        this.currentTime = document.getElementById('current_time');;
+        this.totalTime = document.getElementById('total_time');;
         this.allTracks = [
             {
                 name: 'Уходи',
@@ -88,13 +92,22 @@ class webPlayer {
         this.trackImage.style.backgroundImage = `url(${selectTrack.img})`;
         this.track.load();
 
-        this.currentCount.innerHTML = index + 1;
+        //onLoad track
+        this.track.addEventListener('loadeddata', () => {
+            this.durationSlider.value = 0;
+            this.setIntervalTrack();
+            this.setTotalTime();
+        });
 
+        this.currentCount.innerHTML = index + 1;
+        document.title = `${selectTrack.autor} - ${selectTrack.name}`;
+        
         // setTimeout(() => {
         //     document.getElementById('from').innerHTML = `${parseInt(this.track.duration / 60)}:${parseInt(this.track.duration % 60)}`;
         // }, 200);
-        document.title = `${selectTrack.autor} - ${selectTrack.name}`;
-        this.durationSlider.value = 0;
+    };
+
+    setIntervalTrack () {
         this.timer = setInterval(() => {
             this.rangeDurationSlider();
         }, 1000);
@@ -111,12 +124,14 @@ class webPlayer {
         this.track.play();
         this.playingTrack = true;
         this.play.innerHTML = '<i class="fas fa-pause"></i>';
+        this.setIntervalTrack();
     };
 
     pauseTrack () {
         this.track.pause();
         this.playingTrack = false;
         this.play.innerHTML = '<i class="fas fa-play"></i>';
+        clearInterval(this.timer);
     };
 
     nextTrack () {
@@ -150,9 +165,11 @@ class webPlayer {
     changeDuration () {
         let durationPosition = this.track.duration * (this.durationSlider.value / 100);
         this.track.currentTime = durationPosition;
+        this.setCurrentTime();
     };
 
     rangeDurationSlider () {
+        this.setCurrentTime();
         if (!isNaN(this.track.duration)) {
             this.durationSlider.value = this.track.currentTime * (100 / this.track.duration);
         }
@@ -160,6 +177,24 @@ class webPlayer {
             this.nextTrack();
         }
     };
+
+    setCurrentTime () {
+        let minutes = parseInt(this.track.currentTime / 60);
+        let seconds = parseInt(this.track.currentTime % 60);
+        this.currentTime.innerHTML = this.getNormalTime(minutes, seconds);
+    };
+    
+    setTotalTime () {
+        let minutes = parseInt(this.track.duration / 60);
+        let seconds = parseInt(this.track.duration % 60);
+        this.totalTime.innerHTML = this.getNormalTime(minutes, seconds);
+    };
+
+    getNormalTime (minutes, seconds) {
+        if (+minutes < 10) minutes = `0${minutes}`;
+        if (+seconds < 10) seconds = `0${seconds}`;
+        return `${minutes}:${seconds}`;
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
